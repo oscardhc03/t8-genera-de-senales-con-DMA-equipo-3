98fa67081a1dda5bc6affd988b24c2fa58c19f11
Daniel Sanchez <2399dhc@gmail.com>
2023-04-05 19:55:15 -0600

    Usando matlab genere los valores

--- Senales.h
+++ Senales.h
@@ -7,7 +7,30 @@
 
 #ifndef SENALES_H_
 #define SENALES_H_
+#include "stdint.h"
+#include "bits.h"
 
+#define BUFFER_SIZE 256
+#define DEFAULT_FREQ 500000
+#define MAX_FREQ 1500000
+#define FREQ_STEP 500000
+
+#define PI 3.14159265
+
+typedef enum{
+	SIGNAL_SINE,
+	SIGNAL_TRIANGLE,
+	SIGNAL_SAWTOOTH,
+	SIGNAL_COUNT
+}signal_type_t;
+
+typedef float myfloat;
+
+void Signal_init(void);
+void Signal_NexType(void);
+void Signal_NexFreq(void);
+
+uint16_t *Signals_GetActiveSignalBuffer(void);
 
 
 #endif /* SENALES_H_ */
